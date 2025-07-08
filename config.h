70dad71216991ba11a4fbf9c6d704f1f4d461348
Abiel Almonte <almonteabiel@gmail.com>
2025-07-08 01:31:25 -0400

    use unsigned for fullmask

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
-constexpr uint32_t BLOCKDIM = 512;
+constexpr uint32_t BLOCKDIM = 1024;
 constexpr uint64_t MAXBLOCKS = 1048576;
 
 constexpr uint32_t WARPSIZE = 32;
-constexpr uint64_t FULLMASK = 0xFFFFFFFF;
+constexpr uint64_t FULLMASK = 0xFFFFFFFFu;
