f997f9a93af1ff21fce82c29916f9da16bd43b7d
Abiel Almonte <almonteabiel@gmail.com>
2025-07-08 03:54:21 -0400

    profiling with ncu

--- build.sh
+++ build.sh
@@ -7,3 +7,11 @@ CXX=clang++-17 cmake -S . -B build -DCMAKE_CUDA_COMPILER=clang++-17
 cmake --build build
 
 ./build/hpps "$@"
+
+
+ncu \
+  --set detailed \
+  --print-summary per-kernel \
+  -k single_pass_scan \
+  --log-file roofline_report.log \
+  ./build/hpps 24
